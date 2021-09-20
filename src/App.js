4e941557cdacce53ff3ebd31b567233698ea8e46
Diganta165 <mdsrdiganta@gmail.com>
2021-09-20 12:02:14 +0600

    Displaying Country and Capital

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Countries></Countries>
     </div>
   );
+}
+
+function Countries(){
+// Using useState to set Country Data
+const [countries, setCountries] = useState([]); 
+
+// Using useEffect to load country Data 
+useEffect(()=>{
+  fetch('https://restcountries.eu/rest/v2/all')
+  .then(res => res.json())
+  .then(data => setCountries(data))
+  
+},[]);
+console.log(countries);
+
+
+
+  return(
+    <div>
+      <h1>All The Countries</h1>
+      <h4>Countries Available: {countries.length}</h4>
+      {
+        countries.map(country => <LoadCountry name={country.name}></LoadCountry> )
+      }
+    </div>
+
+
+  );
+}
+
+// Load Country Component 
+function LoadCountry(props){
+// console.log('Props',props);
+return(
+  <div>
+  <h1>Name: {props.name}</h1>
+  <h4>Capital: {props.capital}</h4>
+</div>
+);
 }
 
 export default App;
